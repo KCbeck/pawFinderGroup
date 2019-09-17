969a88131b9357a0cc4103dd1dd7dbc14099b752
Ronald_Becker <ronaldirwinbecker@yahoo.com>
2019-09-17 18:18:33 -0500

    trytest

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,8 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
 import FormPage from "./components/signUp.js"
 import logo from "./logo.png";
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-=======
-// import Form from "./components/form.js"
-import logo from "./logo.png";
-import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import FormPage from "./components/signUp.js";
->>>>>>> 5c5165cf102064cb4bd6d430d20f3d8441e142f4
 
 
 
@@ -23,15 +15,10 @@ class App extends Component {
           <h2>PawFinder</h2>
           <p>Swipe left until you find a Pet you like then swipe right!</p>
         </div>
-<<<<<<< HEAD
-        <FormPage />
-        <div id="contentDiv"></div>
-=======
         <div id="contentDiv">
           <FormPage />
 
         </div>
->>>>>>> 5c5165cf102064cb4bd6d430d20f3d8441e142f4
         <div id="footer"></div>
       </div>
     );
