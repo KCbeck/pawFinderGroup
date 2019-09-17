5c5165cf102064cb4bd6d430d20f3d8441e142f4
Ronald_Becker <ronaldirwinbecker@yahoo.com>
2019-09-17 02:30:36 -0500

    Update signUp.js
    new signup

--- client/src/components/signUp.js
+++ client/src/components/signUp.js
@@ -1,8 +1,8 @@
-// import React from "react";
+import React from "react";
 import { Container, Row, Col, button } from 'react-bootstrap';
-
 import React, {Component} from 'react';
-// import './form.css';
+
+
 // ​
 // ​
 class FormPage extends Component {
