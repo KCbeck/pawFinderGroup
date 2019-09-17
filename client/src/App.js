1c20773d5013e2a531bfd69a45f9d290f7c95239
J-Stu <jennstuart67@gmail.com>
2019-09-17 18:41:51 -0500

    final front end code added to preexisting backend code

--- client/src/App.js
+++ client/src/App.js
@@ -12,11 +12,11 @@ class App extends Component {
       <div id="App">
         <div id="AppHeader">
           <img src={logo} id="AppLogo" alt="logo" />
-          <h2>PawFinder</h2>
+          <h2>PetRoolette</h2>
+          <h3>For the indecisive would-be petowner</h3>
           <p>Swipe left until you find a Pet you like then swipe right!</p>
         </div>
         <div id="contentDiv">
-          <FormPage />
 
         </div>
         <div id="footer"></div>
