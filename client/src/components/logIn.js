28df419d488465660cf9fb74c239efda7b529ecf
J-Stu <jennstuart67@gmail.com>
2019-09-19 02:20:19 -0500

    added browser router and routes in rough manner

---
+++ client/src/components/logIn.js
@@ -0,0 +1,2 @@
+// incorporate <Link to = "/cards"> Submit </Link> into the submit button- Jenn is figuring out how
+// Remember Cards js file needs route to textBox incorporated into right swipe function
