4ac54b76470586341d5c148e388337dfeffd8e97
J-Stu <jennstuart67@gmail.com>
2019-09-19 01:52:15 -0500

    moved p tags so you can read the headline

--- client/src/App.js
+++ client/src/App.js
@@ -13,9 +13,9 @@ class App extends Component {
         <div id="AppHeader">
           <img src={logo} id="AppLogo" alt="logo" />
           <h2>PetRoolette</h2>
-          <h3>For the indecisive would-be petowner</h3>
-          <p>Swipe left until you find a Pet you like then swipe right!</p>
         </div>
+        <p>An app for the indecisive would-be pet owner</p>
+        <p>Swipe left until you find a Pet you like- then swipe right!</p>
         <div id="contentDiv">
         <FormPage />
         </div>
