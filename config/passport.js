fb357fae8b7a94a7b06312ef72de9498b0c57172
J-Stu <jennstuart67@gmail.com>
2019-09-20 18:58:01 -0500

    put Toms passport files in

--- config/passport.js
+++ config/passport.js
@@ -1,111 +1,49 @@
- //load bcrypt
- var bCrypt = require('bcryptjs');
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
 
- module.exports = function(passport,user){
+var db = require("../models");
 
- var User = user;
- var LocalStrategy = require('passport-local').Strategy;
+// Telling passport we want to use a Local Strategy. In other words, we want login with a username/email and password
+passport.use(new LocalStrategy(
+  // Our user will sign in using an email, rather than a "username"
+  {
+    usernameField: "email"
+  },
+  function(email, password, done) {
+    // When a user tries to sign in this code runs
+    db.User.findOne({
+      where: {
+        email: email
+      }
+    }).then(function(dbUser) {
+      // If there's no user with the given email
+      if (!dbUser) {
+        return done(null, false, {
+          message: "Incorrect email."
+        });
+      }
+      // If there is a user with the given email, but the password the user gives us is incorrect
+      else if (!dbUser.validPassword(password)) {
+        return done(null, false, {
+          message: "Incorrect password."
+        });
+      }
+      // If none of the above, return the user
+      return done(null, dbUser);
+    });
+  }
+));
 
+// In order to help keep authentication state across HTTP requests,
+// Sequelize needs to serialize and deserialize the user
+// Just consider this part boilerplate needed to make it all work
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
 
- passport.serializeUser(function(user, done) {
-         done(null, user.id);
-     });
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
 
-
- // used to deserialize the user
- passport.deserializeUser(function(id, done) {
-     User.findById(id).then(function(user) {
-       if(user){
-         done(null, user.get());
-       }
-       else{
-         done(user.errors,null);
-       }
-     });
-
- });
-
-
- passport.use('local-signup', new LocalStrategy(
-
-   {           
-     usernameField : 'email',
-     passwordField : 'password',
-     passReqToCallback : true // allows us to pass back the entire request to the callback
-   },
-
-   function(request, email, password, done){
-      
-
-     var generateHash = function(password) {
-     return bCrypt.hashSync(password, bCrypt.genSaltSync(8), null);
-     };
-
-      User.findOne({where: {email:email}}).then(function(user){
-
-     if(user)
-     {
-       return done(null, false, {message : 'That email is already registered, please login'} );
-     } else {
-       var userPassword = generateHash(password);
-       var data =
-       { 
-       firstname: request.body.firstname,
-       lastname: request.body.lastname,
-       email: email,
-       password: userPassword
-       };
-
-
-       User.create(data).then(function (newUser,created){
-         if(!newUser){
-           return done(null, false);
-         }
-
-         if(newUser){
-           return done(null, newUser);
-         }
-       });
-     }
-   }); 
- }
- ));
-   
- //LOCAL SIGNIN
- passport.use('local-signin', new LocalStrategy({
- // by default, local strategy uses username and password, we will override with email
- usernameField : 'email',
- passwordField : 'password',
- passReqToCallback : true // allows us to pass back the entire request to the callback
- },
-
- function(request, email, password, done) {
-
-   var User = user;
-
-   var isValidPassword = function(userpass , password){
-     return bCrypt.compareSync(password, userpass);
-   }
-
-   User.findOne({ where : { email: email}}).then(function (user) {
-
-     if (!user) {
-       return done(null, false, { message: 'Incorrect Login' });
-     }
-
-     if (!isValidPassword(user.password,password)) {
-       return done(null, false, { message: 'Incorrect Login.' });
-     }
-
-     var userinfo = user.get();
-
-     return done(null,userinfo);
-   }).catch(function(error){
-
-     console.log("Error:",error);
-
-     return done(null, false, { message: 'Something went wrong with your Signin' });
-   });
- }
- ));
- }
+// Exporting our configured passport
+module.exports = passport;
--- server.js
+++ server.js
@@ -1,55 +1,19 @@
-var express    = require('express')
-var app        = express()
-var passport   = require('passport')
-var session    = require('express-session')
-var bodyParser = require('body-parser')
-var exphbs     = require('express-handlebars')
-// require and load .env
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
 
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-//For BodyParser
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-
- // For Passport
-app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-
-
- //For Handlebars
-app.set('views', './views')
-app.engine('hbs', exphbs({extname: '.hbs'}));
-app.set('view engine', '.hbs');
-
-
-app.get('/', function(req, res){
-  res.send('Welcome ');
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(request, response) {
+  response.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
-
-//Models
-var models = require("./models");
-
-
-// //Routes
-// var authRoute = require('./routes/auth')(app,passport);
-
-//load passport strategies
-require('./config/passport.js')(passport,models.user);
-
-
-//Sync Database
-models.sequelize.sync().then(function(){
-console.log('Nice! Database up and on')
-
-}).catch(function(err){
-console.log(err,"Something went wrong with the Database Update!")
-});
-
-app.listen(5000, function(err){
-    if(!err)
-    console.log("Site is live"); else console.log(err)
-
+app.listen(PORT, function() {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
