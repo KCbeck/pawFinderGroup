425a4d8c42a283c40965b4216c93582130c65e22
J-Stu <jennstuart67@gmail.com>
2019-09-20 08:43:59 -0500

    had to tweak packages again but have both server side and client side up as of this commit

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var app = express();
+var passport   = require('passport')
+var session    = require('express-session')
+var bodyParser = require('body-parser') 
  
- 
+//For BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); 
+
+// For Passport
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+
 app.get('/', function(request, response) {
- 
     response.send('Welcome to Passport with Sequelize');
- 
 });
  
  
