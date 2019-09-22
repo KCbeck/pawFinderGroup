b1cde5fe3763b6611c61fd93d0ba6ade8f906136
Ronald_Becker <ronaldirwinbecker@yahoo.com>
2019-09-21 19:28:23 -0500

    tryingagain

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
+
 import React, { Component } from "react";
-import { BrowserRouter, Route } from 'react-router-dom';
+import "./App.css";
 import logo from "./logo.png";
 import roolettBackground from "./roolettBackground.jpg";
-import "./App.css";
 import FormPage from "./components/signUp.js";
 import textBox from './components/textBox.js';
 import logIn from './components/logIn';
-import axios from 'axios';
 import PetCard from './components/card.js';
-
+import axios from 'axios';
+import {  BrowserRouter as Router, Link, Route  } from 'react-router-dom';
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import Routes from "./Routes";
 
 class App extends Component {
 
@@ -34,36 +37,57 @@ cardStyle = {
   width: '25vw'
 
 }
-   render() {
-    return (
-      <div id="App">
-        <div id="AppHeader">
-          <img src={logo} id="AppLogo" alt="logo" />
-          <img src={roolettBackground} id="roolettBackground" alt="roolettBackground" />
-          <h2 id="PRheader">PetRoolette</h2>
-        </div>
-        <div id="appdescription">
-          <p >An app for the indecisive would-be pet owner</p>
-          <p>Swipe left until you find a Pet you like- then swipe right!</p>
-        </div>
-        <BrowserRouter>
-          <div className="contentDiv">
-            <Route exact path="/" component={ FormPage } />
-            <Route exact path="/login" component= { logIn } />
-            <Route exact path="/confirmation" component= { textBox } />
-            <div style={this.rowStyle}> 
-              {this.state.animals.map(animal => 
-                <div >
-                  <div style={this.cardStyle} className="col-lg-3"><PetCard name={animal.name} image={animal.photos[0] ? animal.photos[0].small : ''} species={animal.species} size={animal.size} gender={animal.gender} breed={animal.breeds.primary} /></div>
-                </div>
-              
-              )}
+render() {
+  return (
+        <div id="App">
+            <div id="AppHeader">
+            <img src={logo} id="AppLogo" alt="logo" />
+            <img src={roolettBackground} id="roolettBackground" alt="roolettBackground" />
+            <h2 id="PRheader">PetRoolette</h2>
             </div>
-          </div>
-        </BrowserRouter>
-      </div>
-    );
-  }
+          
+            {/* <div id="appdescription"> */}
+              <div className="App container">
+                <Navbar fluid collapseOnSelect>
+                <div>
+                  <Navbar.Brand>
+                    <Link to="/">PetRoolett</Link>
+                    </Navbar.Brand> 
+                  <Navbar.Collapse>
+                    <Nav pullRight>
+                        <Navbar.Toggle />
+                           <LinkContainer to="/signup">
+                            <NavItem>Signup</NavItem>
+                            </LinkContainer>
+                            
+                            <LinkContainer to="/login">
+                            <NavItem>Login</NavItem>
+                            </LinkContainer>
+                         </Nav>
+                   </Navbar.Collapse>
+                   </div>                
+                  </Navbar>
+                <Routes />
+              
+                <Router>
+                    <div className="contentDiv">
+                      <Route exact path="/" component={ FormPage } />
+                      <Route exact path="/login" component= { logIn } />
+                      <Route exact path="/confirmation" component= { textBox } />
+                      <div style={this.rowStyle}> 
+                        {this.state.animals.map(animal => 
+                          <div >
+                            <div style={this.cardStyle} className="col-lg-3"><PetCard name={animal.name} image={animal.photos[0] ? animal.photos[0].small : ''} species={animal.species} size={animal.size} gender={animal.gender} breed={animal.breeds.primary} /></div>
+                          </div>
+                        )}
+                      </div>
+                      </div>
+                </Router>
+            </div>
+        </div>
+  );
+}
+
 }
 
 export default App;
---
+++ client/src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>PetRoolette</h1>
+          <p>Pet App</p>
+        </div>
+      </div>
+    );
+  }
+}
